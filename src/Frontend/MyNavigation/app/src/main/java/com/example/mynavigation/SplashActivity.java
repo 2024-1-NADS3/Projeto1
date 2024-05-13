abae84335d6ae02fa4f3c333e3ecdaf2f24ff96c
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-05-12 21:42:05 -0300

    slider

--- src/Frontend/MyNavigation/app/src/main/java/com/example/mynavigation/SplashActivity.java
+++ src/Frontend/MyNavigation/app/src/main/java/com/example/mynavigation/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                    startActivity(new Intent(SplashActivity.this,login.class));
+                    startActivity(new Intent(SplashActivity.this,Slider.class));
                 finish();
             }
         },1000);

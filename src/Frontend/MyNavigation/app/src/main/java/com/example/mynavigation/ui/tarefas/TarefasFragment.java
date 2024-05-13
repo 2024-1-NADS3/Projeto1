8ed9905dd26627b5c4dc40be2801348f0705378d
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-05-12 22:12:10 -0300

    listView

--- src/Frontend/MyNavigation/app/src/main/java/com/example/mynavigation/ui/tarefas/TarefasFragment.java
+++ src/Frontend/MyNavigation/app/src/main/java/com/example/mynavigation/ui/tarefas/TarefasFragment.java
@@ -1,21 +1,32 @@
 package com.example.mynavigation.ui.tarefas;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.fragment.app.Fragment;
 import com.example.mynavigation.R;
 
 public class TarefasFragment extends Fragment {
 
+    private ListView listaTarefas;
+    private String[] itens = {
+            "Exemplo1", "Exemplo2", "Exemplo3", "Exemplo4", "Exemplo5"
+    };
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tarefas, container, false);
+        View view = inflater.inflate(R.layout.fragment_tarefas, container, false);
+
+
+        listaTarefas = view.findViewById(R.id.listaTarefas);
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, itens);
+        //ArrayAdapter<String> adaptador = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, itens);
+        listaTarefas.setAdapter(adaptador);
+
+        return view;
     }
 }

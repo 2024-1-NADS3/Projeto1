72a35e040a4683ae441e84da47f5df25c25dcd3d
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-05-14 17:33:29 -0300

    lendo json teste com listView

--- src/Frontend/MyNavigation/app/src/main/java/com/example/mynavigation/ui/tarefas/TarefasFragment.java
+++ src/Frontend/MyNavigation/app/src/main/java/com/example/mynavigation/ui/tarefas/TarefasFragment.java
@@ -9,24 +9,54 @@ import android.widget.ListView;
 import androidx.fragment.app.Fragment;
 import com.example.mynavigation.R;
 
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class TarefasFragment extends Fragment {
 
     private ListView listaTarefas;
-    private String[] itens = {
-            "Exemplo1", "Exemplo2", "Exemplo3", "Exemplo4", "Exemplo5"
-    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tarefas, container, false);
 
-
         listaTarefas = view.findViewById(R.id.listaTarefas);
-        ArrayAdapter<String> adaptador = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, itens);
-        //ArrayAdapter<String> adaptador = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, itens);
+
+        // Parse do JSON e extrair as tarefas
+        String json = "{ \"tasksList\": [ { \"_id\": \"663d090d6f0b63e80fc58903\", \"task\": \"Estudar Muito Node\", \"date\": \"2024-05-09T17:34:05.226Z\", \"__v\": 0 }," +
+                " { \"_id\": \"663d21c7207389c71e3c6ff8\", \"task\": \"Ir para a academia\", \"date\": \"2024-05-09T19:19:35.647Z\", \"__v\": 0 }," +
+                " { \"_id\": \"663d21dd207389c71e3c6ffb\", \"task\": \"Comprar comida para o gato\", \"date\": \"2024-05-09T19:19:57.861Z\", \"__v\": 0 }," +
+                " { \"_id\": \"663d23820a799d815e71a318\", \"task\": \"TESTE\", \"date\": \"2024-05-09T19:26:58.345Z\", \"__v\": 0 }, " +
+                "{ \"_id\": \"663d29058165bf19dbbbbc31\", \"task\": \"TESTE deletar 2\", \"date\": \"2024-05-09T19:50:29.862Z\", \"__v\": 0 }, " +
+                "{ \"_id\": \"663d5dc3b84c0514bdbf997e\", \"task\": \"TESTE deletar 10\", \"date\": \"2024-05-09T23:35:31.167Z\", \"__v\": 0 } ] }";
+        ArrayList<String> tarefas = parseJSON(json);
+
+        // Atualizar o ArrayAdapter
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, tarefas);
         listaTarefas.setAdapter(adaptador);
 
         return view;
+    }
+
+    // Método para analisar o JSON e extrair as tarefas
+    private ArrayList<String> parseJSON(String json) {
+        ArrayList<String> tarefas = new ArrayList<>();
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            JSONArray tasksList = jsonObject.getJSONArray("tasksList");
+            for (int i = 0; i < tasksList.length(); i++) {
+                JSONObject taskObj = tasksList.getJSONObject(i);
+                String task = taskObj.getString("task");
+                tarefas.add(task);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return tarefas;
     }
 }

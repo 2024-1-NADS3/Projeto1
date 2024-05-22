3806e38c5167aa779bc3d6c34019d7f83d7d9115
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-05-22 15:34:12 -0300

    Layout arrumado

--- src/Frontend/MyNavigation/app/src/main/java/com/example/mynavigation/ui/tarefasAtrasadas/TarefasAtrasadasFragment.java
+++ src/Frontend/MyNavigation/app/src/main/java/com/example/mynavigation/ui/tarefasAtrasadas/TarefasAtrasadasFragment.java
@@ -1,6 +1,14 @@
 package com.example.mynavigation.ui.tarefasAtrasadas;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
+
+import com.example.mynavigation.R;
+import com.example.mynavigation.ui.progresso.ProgressoFragment;
 
 public class TarefasAtrasadasFragment extends Fragment {
     //Calendario
@@ -8,6 +16,19 @@ public class TarefasAtrasadasFragment extends Fragment {
     public TarefasAtrasadasFragment() {
         // Required empty public constructor
     }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tarefasatrasadas, container, false);
+
+        return view;
+    }
+
+
 
 }
 

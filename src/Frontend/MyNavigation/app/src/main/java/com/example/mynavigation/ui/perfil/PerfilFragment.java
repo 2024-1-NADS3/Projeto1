5babf63f1619c1b0ff0962a30237f47ab482b104
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-05-22 20:57:28 -0300

    botão alterado

--- src/Frontend/MyNavigation/app/src/main/java/com/example/mynavigation/ui/perfil/PerfilFragment.java
+++ src/Frontend/MyNavigation/app/src/main/java/com/example/mynavigation/ui/perfil/PerfilFragment.java
@@ -10,12 +10,9 @@ import androidx.fragment.app.Fragment;
 import com.example.mynavigation.R;
 
 
-import android.widget.TextView;
+public class PerfilFragment extends Fragment {
 
-
-public class TarefasAtrasadasFragment extends Fragment {
-
-    public TarefasAtrasadasFragment() {
+    public PerfilFragment() {
         // Required empty public constructor
     }
 
@@ -27,7 +24,7 @@ public class TarefasAtrasadasFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_tarefasatrasadas, container, false);
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
 
         return view;
     }

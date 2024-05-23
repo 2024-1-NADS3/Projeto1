d2ac4264cfbba8a89ee281b808ec745ed58f028b
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-05-23 03:44:16 -0300

    usando Thread para aguardar obtenção do total

--- src/Frontend/MyNavigation/app/src/main/java/com/example/mynavigation/ui/progresso/ProgressoFragment.java
+++ src/Frontend/MyNavigation/app/src/main/java/com/example/mynavigation/ui/progresso/ProgressoFragment.java
@@ -6,12 +6,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.graphics.LinearGradient;
-import android.graphics.Shader;
 import android.widget.ArrayAdapter;
 
 import com.android.volley.Response;
@@ -31,26 +28,20 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
 
 import com.android.volley.Request;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 
-
-
 public class ProgressoFragment extends Fragment {
     private ProgressBar progressBar;
     private int idUsuario = ClasseUsuarioLogado.getIdUsuarioLogado();
-
     private TextView textTotalTarefa, textPorcentagem;
     private ListView listaTarefasFeitas;
 
     private static final String BASE_URL = "https://vq4x7v-3000.csb.app";
-
-    private int progress = 0;
     private int total;
-
-
 
     public ProgressoFragment() {
         // Required empty public constructor
@@ -59,37 +50,36 @@ public class ProgressoFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_progresso, container, false);
 
-
-        // Barra de progresso
+        // Inicialização das views
         progressBar = view.findViewById(R.id.progressBar2);
         textTotalTarefa = view.findViewById(R.id.textTotalTarefa);
         textPorcentagem = view.findViewById(R.id.textPorcentagem);
-        getTotalTasks(idUsuario);
-
-
-
-        //Trazendo lista de tarefas
         listaTarefasFeitas = view.findViewById(R.id.listaTarefasFeitas);
 
-        ProgressoFragment.MyTasks task = new ProgressoFragment.MyTasks();
-        //String urlApi = "https://vq4x7v-3000.csb.app/obterTarefasAtivas/" + idUsuario;
-        String urlApi = "https://vq4x7v-3000.csb.app/obterTarefasFinalizadas/" + idUsuario;
-        task.execute(urlApi);
+      
+        CountDownLatch latch = new CountDownLatch(1);
+
+        getTotalTasks(idUsuario, latch);
+
+        // Espera pela conclusão da obtenção do total de tarefas
+        new Thread(() -> {
+            try {
+                latch.await();
+                // Após obter o total de tarefas, buscar as tarefas concluídas
+                getCompletedTasks(idUsuario);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
 
         return view;
     }
-
-
-
-
 
     // Método para fazer o parse do JSON e extrair os nomes das tarefas
     private ArrayList<String> parseJSON(String json) {
@@ -107,8 +97,14 @@ public class ProgressoFragment extends Fragment {
         return listaDeTarefas;
     }
 
-    class MyTasks extends AsyncTask<String, Void, String> {
+    // Método para obter tarefas concluídas
+    private void getCompletedTasks(int idUsuario) {
+        String urlApi = BASE_URL + "/obterTarefasFinalizadas/" + idUsuario;
+        ProgressoFragment.MyTasks task = new ProgressoFragment.MyTasks();
+        task.execute(urlApi);
+    }
 
+    class MyTasks extends AsyncTask<String, Void, String> {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -118,7 +114,6 @@ public class ProgressoFragment extends Fragment {
         protected String doInBackground(String... strings) {
             String stringUrl = strings[0];
             StringBuilder result = new StringBuilder();
-
             try {
                 URL url = new URL(stringUrl);
                 HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
@@ -134,13 +129,11 @@ public class ProgressoFragment extends Fragment {
                 reader.close();
                 inputStreamReader.close();
                 inputStream.close();
-
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
             return result.toString();
         }
 
@@ -148,18 +141,12 @@ public class ProgressoFragment extends Fragment {
         protected void onPostExecute(String resultado) {
             super.onPostExecute(resultado);
             ArrayList<String> tarefas = parseJSON(resultado);
-            ArrayAdapter<String> adaptador = new ArrayAdapter<>(getActivity(),
-                    android.R.layout.simple_list_item_1,
-                    tarefas);
-
-            // Atualização da ListView com adaptador
+            ArrayAdapter<String> adaptador = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, tarefas);
             listaTarefasFeitas.setAdapter(adaptador);
 
-            //Definindo tarefas feitaspara o progresso e passando como parametro
             int feitas = adaptador.getCount();
             updateProgress(total, feitas);
         }
-
 
         private void updateProgress(int total, int feitas) {
             textTotalTarefa.setText(feitas + "/" + total);
@@ -171,34 +158,31 @@ public class ProgressoFragment extends Fragment {
             // Atualiza a barra de progresso
             progressBar.setProgress(porcentagem);
         }
-
     }
 
-    private void getTotalTasks(int idUsuario) {
+    private void getTotalTasks(int idUsuario, CountDownLatch latch) {
         String url = BASE_URL + "/quantidadeTarefas/" + idUsuario;
-
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            //int total = response.getInt("total");
-                            //textTotalTarefa.setText(total);
                             total = response.getInt("total");
                         } catch (JSONException e) {
                             e.printStackTrace();
+                        } finally {
+                            latch.countDown(); // Reduz a contagem do latch
                         }
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-
                         error.printStackTrace();
+                        latch.countDown(); // Reduz a contagem do latch mesmo em caso de erro
                     }
                 });
 
-        // Adiciona a requisição à fila do Volley
         Volley.newRequestQueue(getActivity().getApplicationContext()).add(request);
     }
 }
